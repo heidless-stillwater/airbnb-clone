c26e79cadc95c70ba6e5e76870287033d708da48
heidless <lockhart.r@gmail.com>
2024-09-26 12:43:20 +0100

    form now hooked up

--- airbnb-app-1/config/routes.rb
+++ airbnb-app-1/config/routes.rb
@@ -15,5 +15,6 @@ Rails.application.routes.draw do
 
   namespace :api do 
     resources :users, only: :show
+    get "/users_by_email" => "users_by_email#show", as: :users_by_email, format: :json
   end
 end
--- airbnb-app-1/spec/requests/api/users_by_email_spec.rb
+++ airbnb-app-1/spec/requests/api/users_by_email_spec.rb
@@ -2,24 +2,23 @@
 
 require 'rails_helper'
 
-RSpec.describe "Api::Users", type: :request do 
+RSpec.describe "Api::UsersByEmail", type: :request do 
   describe "GET show" do 
     let(:headers) do 
       { "ACCEPT" => "application/json" }
     end
     context "user exists" do 
       it "is successful" do 
-
         user = create(:user)
-        
-        get api_user_path(id: user), headers: headers
+
+        get api_users_by_email_path, params: {email: user.email}, headers: headers
         expect(response).to be_successful
       end
     end
 
     context "user does not exist" do 
       it "is not found" do 
-        get api_user_path(id: "junk"), headers: headers
+        get api_users_by_email_path, params: {email: "junk@junk.com"}, headers: headers
         # expect(response).not_to be_successful     # WORKS
         expect(response.status).to eq 404           # WORKS
       end
